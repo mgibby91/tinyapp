b429a81383c0af8524f4e11fcc61c12ec3060db1
Matthew Gibson <mattgibson6@gmail.com>
2020-08-03 19:53:52 +0000

    Modified express_server.js server to include route for /hello

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = 8081;
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get('/hello', (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 })
 
 app.listen(PORT, () => {
