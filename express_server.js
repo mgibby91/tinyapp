ff636d806180ba6fde3b73aff9d60a6a361f1da8
Matthew Gibson <mattgibson6@gmail.com>
2020-08-03 15:30:30 -0600

    added header partials to both ejs files

--- express_server.js
+++ express_server.js
@@ -26,6 +26,9 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+
+
+
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 })
