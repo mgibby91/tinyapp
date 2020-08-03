023ddd52c1ebc3f16157ce65e72483533ea1dcf6
Matthew Gibson <mattgibson6@gmail.com>
2020-08-03 17:04:51 -0600

    modified the express server to redirect to the longURL page when clicking the short URL page using a new get request

--- express_server.js
+++ express_server.js
@@ -47,7 +47,12 @@ app.post('/urls', (req, res) => {
   };
   res.render('urls_show', templateVars);
 
-})
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 
 
 
